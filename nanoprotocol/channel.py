482d7eb5487fde974dacfee706077141dbf073ec
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-02-07 19:47:05 +0100

    async

--- nanoprotocol/channel.py
+++ nanoprotocol/channel.py
@@ -1,104 +1,104 @@
+import asyncio
 import argparse
-import socket
-from ast import arg
 from dataclasses import dataclass
-from email import message
 from random import randbytes
 
 import ed25519_blake2b
 
 from . import messages
-from .blocks import BlockWrapper
-from .messages import MessageHandshake, MessageHeader, MessagePayload, MessageWrapper
+from .blocks import *
+from .messages import *
 
 
-def read_exact(sock: socket.socket, size: int) -> bytes:
+async def read_exact(reader, size: int) -> bytes:
     data = b""
     while len(data) < size:
-        data += sock.recv(size - len(data))
-        # print("read:", len(data))
+        chunk = await reader.read(size - len(data))
+        if not chunk:
+            raise ConnectionError("Socket connection broken")
+        data += chunk
     return data
 
 
 @dataclass
 class Channel:
-    sock: socket.socket
+    reader: asyncio.StreamReader
+    writer: asyncio.StreamWriter
 
-    def send(self, message: MessagePayload):
-        # print("sending:", message)
+    async def send(self, message: MessagePayload):
         wrapped = messages.wrap_message(message)
         data = messages.serialize_message(wrapped)
-        self.sock.sendall(data)
+        self.writer.write(data)
+        await self.writer.drain()
 
-    def receive(self) -> MessagePayload:
-        header_data = read_exact(self.sock, MessageHeader.SIZE)
+    async def receive(self) -> MessagePayload:
+        header_data = await read_exact(self.reader, MessageHeader.SIZE)
         assert len(header_data) == MessageHeader.SIZE
 
         header, message_size = messages.deserialize_header(header_data)
-        # print("message size:", message_size)
 
-        message_data = read_exact(self.sock, message_size)
+        message_data = await read_exact(self.reader, message_size)
         assert len(message_data) == message_size
 
         message = messages.deserialize_message(message_data, header)
         return message.payload
 
-    def publish_block(self, block: BlockWrapper):
-        # print("publish block:", block)
+    async def publish_block(self, block: BlockWrapper):
         publish = messages.MessagePublish(block)
-        self.send(publish)
+        await self.send(publish)
 
     @staticmethod
-    def connect(address: str, port: int) -> "Channel":
-        sock = socket.create_connection((address, port))
-        # print("socket:", sock)
+    async def connect(address: str, port: int) -> "Channel":
+        reader, writer = await asyncio.open_connection(address, port)
+        channel = Channel(reader, writer)
 
-        channel = Channel(sock)
-
-        def create_query_response(cookie: bytes):
+        async def create_query_response(cookie: bytes):
             signing_key, verifying_key = ed25519_blake2b.create_keypair()
             signature = signing_key.sign(cookie)
             return verifying_key.to_bytes(), signature
 
-        def handshake():
+        async def handshake():
             query = randbytes(32)
             local_query = MessageHandshake(query=query)
-            channel.send(local_query)
+            await channel.send(local_query)
 
-            remote_response = channel.receive()
-            # we need to complete remote server query to establish realtime channel
+            remote_response = await channel.receive()
             assert isinstance(remote_response, MessageHandshake)
             assert remote_response.query
-            # print("handshake response:", remote_response)
 
-            account, signature = create_query_response(remote_response.query)
+            account, signature = await create_query_response(remote_response.query)
             local_response = MessageHandshake(response=MessageHandshake.Response(account, signature))
-            channel.send(local_response)
+            await channel.send(local_response)
 
-        handshake()
-        # print("handshake done")
+        await handshake()
 
         return channel
 
 
-if __name__ == "__main__":
+async def main():
+    import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--address", type=str, default="localhost")
     parser.add_argument("--port", type=int, default=17075)
     args = parser.parse_args()
 
-    def test_channel(address, port):
-        channel = Channel.connect(address, port)
+    async def test_channel(address, port):
+        channel = await Channel.connect(address, port)
+        print("connected:", channel)
 
         handshake = MessageHandshake(query=randbytes(32), response=None)
-        print("sent:", handshake)
 
-        channel.send(handshake)
+        print("sending handshake...")
+        await channel.send(handshake)
+        print("sent handshake")
 
         while True:
-            response = channel.receive()
-            print("response:", response)
+            response = await channel.receive()
+            print("received:", response)
 
-    test_channel(args.address, args.port)
+    await test_channel(args.address, args.port)
 
-    pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
